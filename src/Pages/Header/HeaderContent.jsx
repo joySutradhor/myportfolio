966bf1a8d2a2ad935667368a9626f76b9fb8feee
joySutradhor <joysutradhorcmt@gmail.com>
2024-05-04 01:33:55 +0600

    modal alert open

--- src/Pages/Header/HeaderContent.jsx
+++ src/Pages/Header/HeaderContent.jsx
@@ -3,10 +3,24 @@ import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 // icons
 import { FaPlay } from "react-icons/fa";
-import {Fade, Zoom } from 'react-awesome-reveal';
+import { Fade, Zoom } from 'react-awesome-reveal';
+import { useState } from 'react';
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 
 
 const HeaderContent = () => {
+
+    const [ModalOpen, setModalOpen] = useState(false)
+
+    // show modal funcitons 
+    const showModal = () => {
+        setModalOpen(true)
+    }
+
+    const handleClose = () => {
+        setModalOpen(false)
+    }
     // code editor text 
     const codeString =
         `
@@ -33,8 +47,8 @@ import { dark } from '/dist/esm/styles/prism';
                             </div>
 
                             <div>
-                                <Fade  delay={2000} direction='left'>
-                                    <button className='flex space-x-1 items-center'> <span><FaPlay></FaPlay></span><span>Run</span></button>
+                                <Fade delay={2000} direction='left'>
+                                    <button className='flex space-x-1 items-center' > <span ><FaPlay onClick={showModal}></FaPlay></span><span>Run</span></button>
                                 </Fade >
                             </div>
                         </div>
@@ -50,6 +64,52 @@ import { dark } from '/dist/esm/styles/prism';
                     </SyntaxHighlighter> </Zoom>
                 </div>
             </div>
+            {
+                ModalOpen && <div className='modalParent xl:pt-[50px] '>
+                    <div className='flex justify-between items-center  '>
+                        <div className=''>
+                            <h1 className='mainHeading'>Joy Sutradhor</h1>
+                            <p className='secondaryHeading '>Front End Developer</p>
+                        </div>
+                        <p className='text-red-500  ' onClick={handleClose}><RiDeleteBin6Line className='text-white cursor-pointer hover:text-red-600'></RiDeleteBin6Line></p>
+                    </div>
+                    <div className='gameParent'>
+                        <div>
+                            <div className='border h-[60vh] bg-black border-gray-900'>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div className='grid grid-cols-3 gap-[20px] text-white text-[18px]'>
+                                <button className='border-[1px] border-transparent hover:border-[1px] hover:border-gray-400'>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                                <button>Play video </button>
+                            </div>
+                                <div className='w-full bg-red-400 flex justify-center items-center mt-[30px]'>
+                                    <button className='w-full text-center'>Reset video </button> 
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
 };
